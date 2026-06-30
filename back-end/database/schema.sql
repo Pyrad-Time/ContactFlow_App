071e92840eb5631cb90efc5db2afb0580dd21e29
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-30 09:08:17 -0300

    feat(db): add updated at trigger

--- back-end/database/schema.sql
+++ back-end/database/schema.sql
@@ -36,3 +36,18 @@ CREATE TABLE IF NOT EXISTS contact_interactions (
     content TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS update_contacts_updated_at ON contacts;
+
+CREATE TRIGGER update_contacts_updated_at
+BEFORE UPDATE ON contacts
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
