36fd24ca91ce50e5d18b1895017e062ee2670a50
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-30 08:51:21 -0300

    feat(db): add constrainsts for source and status

--- back-end/database/schema.sql
+++ back-end/database/schema.sql
@@ -1,12 +1,30 @@
 CREATE TABLE IF NOT EXISTS contacts (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    phone TEXT UNIQUE,
+    email TEXT UNIQUE,
+    phone TEXT,
     company TEXT,
     role TEXT,
     source TEXT NOT NULL DEFAULT 'other',
     status TEXT NOT NULL DEFAULT 'new',
     notes TEXT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    CHECK (source IN(
+        'linkedin',
+        'whatsapp',
+        'instagram',
+        'referral',
+        'event',
+        'website',
+        'other'
+    )),
+    CHECK (status IN (
+        'new',
+        'in_contact',
+        'client',
+        'partner',
+        'archived'
+    ))
+);
