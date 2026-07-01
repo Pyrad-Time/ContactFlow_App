b3b2a5b76c66670bef200f8f7b0208a4c056c550
Pyrad-Time <marcos0fataak@gmail.com>
2026-07-01 10:09:51 -0300

    feat(contacts): add create contact repository

---
+++ back-end/repositories/contactRepositories.js
@@ -0,0 +1,42 @@
+import { query } from "../database/connection.js"
+
+export async function createContact(contactData) {
+    const {
+        name, 
+        email,
+        phone,
+        company,
+        role,
+        source,
+        status,
+        notes
+    } = contactData
+
+    const result = await query(
+        `
+        INSERT INTO contacts(
+            name, 
+            email, 
+            phone,
+            company,
+            role,
+            source,
+            status,
+            notes
+        )
+        VALUES ($1, $2,$3,$4,$5,$6,$7,$8)
+        RETURNING *
+        `, [
+            name, 
+            email,
+            phone,
+            company,
+            role,
+            source,
+            status,
+            notes
+        ]
+    )
+
+    return result.rows[0]
+}
