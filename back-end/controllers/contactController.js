02c985fc3ad5be2633113ea604267c46397f475a
Pyrad-Time <marcos0fataak@gmail.com>
2026-07-16 09:48:30 -0300

    feat(contacts): add list contacts controller

--- back-end/controllers/contactController.js
+++ back-end/controllers/contactController.js
@@ -1,4 +1,14 @@
-import { createContact } from "../repositories/contactRepositories.js";
+import { createContact, findAllContacts } from "../repositories/contactRepositories.js";
+
+export async function findAllContactsController(req, res) {
+    try {
+        const allContacts = await findAllContacts()
+
+        return res.status(200).json(allContacts)
+    } catch(error) {
+        return res.status(500).json({ message: "Internal serrver error" })
+    }
+}
 
 export async function createContactController(req, res){
     try {
