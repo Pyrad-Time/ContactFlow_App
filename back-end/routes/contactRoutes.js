baac2eaa908695bb226054b91b5a3b251197f1ed
Pyrad-Time <marcos0fataak@gmail.com>
2026-07-18 16:09:02 -0300

    feat(contact): add delete contact route

--- back-end/routes/contactRoutes.js
+++ back-end/routes/contactRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { createContactController, findAllContactsController, findContactByIdController, updateContactController } from "../controllers/contactController.js"
+import { createContactController, deleteContactController, findAllContactsController, findContactByIdController, updateContactController } from "../controllers/contactController.js"
 
 const router = express.Router()
 
 router.get("/", findAllContactsController)
 router.get("/:id", findContactByIdController)
 router.post("/", createContactController)
+router.delete("/:id", deleteContactController)
 router.patch("/:id", updateContactController)
 
 export default router
