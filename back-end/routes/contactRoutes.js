4f13b4d57db906462ac1a3c5d0314a576b0b35d8
Pyrad-Time <marcos0fataak@gmail.com>
2026-07-18 15:25:50 -0300

    feat(contact): add update contact route

--- back-end/routes/contactRoutes.js
+++ back-end/routes/contactRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createContactController, findAllContactsController, findContactByIdController } from "../controllers/contactController.js"
+import { createContactController, findAllContactsController, findContactByIdController, updateContactController } from "../controllers/contactController.js"
 
 const router = express.Router()
 
 router.get("/", findAllContactsController)
 router.get("/:id", findContactByIdController)
 router.post("/", createContactController)
+router.patch("/:id", updateContactController)
 
 export default router
