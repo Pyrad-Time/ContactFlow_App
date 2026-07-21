68604c316455c9d29879460a7dce2884b687275f
Pyrad-Time <marcos0fataak@gmail.com>
2026-07-21 11:18:50 -0300

    feat(interactions): add delete contact interaction controller

--- back-end/controllers/interactionController.js
+++ back-end/controllers/interactionController.js
@@ -1,4 +1,4 @@
-import { createInteraction } from "../repositories/interactionRepositories.js";
+import { createInteraction, deleteInteraction } from "../repositories/interactionRepositories.js";
 
 export async function createInteractionController(req, res) {
     try {
@@ -14,5 +14,21 @@ export async function createInteractionController(req, res) {
     return res.status(201).json(createdInteraction)
     } catch (error) {
         res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+export async function deleteInteractionController(req, res) {
+    try {
+        const { id, interactionId } = req.params
+
+        const deletedInteraction = await deleteInteraction(id, interactionId)
+
+        if(!deleteInteraction) {
+            res.status(400).json({ message: "Interaction not found"})
+        }
+
+        return res.status(200).json(deletedInteraction)
+    } catch(error) {
+        return res.status(500).json({ message: "Internal server error" })
     }
 }
