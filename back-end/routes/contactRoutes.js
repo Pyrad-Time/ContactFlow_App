1eda73523ab4362b4ef27e1577aa63f1c1441804
Pyrad-Time <marcos0fataak@gmail.com>
2026-07-22 09:21:47 -0300

    fix(route): add slash on get route interaction

--- back-end/routes/contactRoutes.js
+++ back-end/routes/contactRoutes.js
@@ -1,13 +1,13 @@
 import express from "express"
 import { createContactController, deleteContactController, findAllContactsController, findContactByIdController, updateContactController } from "../controllers/contactController.js"
-import { createInteractionController, deleteInteractionController, findInteractionByIdController } from "../controllers/interactionController.js"
+import { createInteractionController, deleteInteractionController, findInteractionByContactIdController } from "../controllers/interactionController.js"
 
 
 const router = express.Router()
 
 router.get("/", findAllContactsController)
 router.get("/:id", findContactByIdController)
-router.get(":id/interactions", findInteractionByIdController)
+router.get("/:id/interactions", findInteractionByContactIdController)
 router.post("/", createContactController)
 router.post("/:id/interactions", createInteractionController)
 router.delete("/:id", deleteContactController)
