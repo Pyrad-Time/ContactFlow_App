56c33982d802827c53fb064dbe309b62586c165a
Pyrad-Time <marcos0fataak@gmail.com>
2026-07-29 10:04:37 -0300

    feat(dashboard): fetch dashboard data

--- front-end/src/pages/Dashboard/index.jsx
+++ front-end/src/pages/Dashboard/index.jsx
@@ -1,8 +1,51 @@
+import { useEffect, useState } from "react"
+import { getDashboardData } from "../../services/dashboardService"
+
 export function Dashboard() {
+    const [dashboardData, setDashboardData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [err, setErr] = useState(null)
+
+    useEffect(() => {
+        async function fetchDashboardData() {
+            try {
+            const data = await getDashboardData()
+
+            setDashboardData(data)
+        } catch(error){
+            setErr(error.message)
+        } finally {
+            setIsLoading(false)
+        }
+        }
+
+        fetchDashboardData()
+    }, [])
+
+        if(isLoading) {
+            return (
+                <main>
+                    <h1>Dashboard</h1>
+                    <p>Carregando...</p>
+                </main>
+            )
+        }
+
+        if(err) {
+            return (
+                <main>
+                    <h1>Dashboard</h1>
+                    <p>Erro: {err}</p>
+                </main>
+            )
+        }
+
     return (
         <main>
             <h1>Dashboard</h1>
-            <p>ContactFlow Dashboard page</p>
+            <pre>
+                {JSON.stringify(dashboardData, null, 2)}
+            </pre>
         </main>
     )
 }
