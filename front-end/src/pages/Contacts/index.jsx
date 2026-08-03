fc24b0420e5b999ef2831e7222d91ad4b60cdef1
Pyrad-Time <marcos0fataak@gmail.com>
2026-08-03 09:27:21 -0300

    feat(contact): add contacts data flow

--- front-end/src/pages/Contacts/index.jsx
+++ front-end/src/pages/Contacts/index.jsx
@@ -1,7 +1,60 @@
+import { useEffect, useState } from "react"
+import { getContacts } from "../../services/contactService"
+import { ContactCard } from "../../components/Contacts/ContactCard"
+
+
 export function Contacts() {
+    const [contacts, setContacts] = useState([])
+    const [ isLoading, setIsLoading] = useState(true)
+    const [ err, setErr] = useState(null)
+
+    useEffect(() => {
+        async function fetchContactsData() {
+            try {
+            const data = await getContacts()
+
+            setContacts(data)
+        } catch(error) {
+            setErr(error.message)
+        } finally {
+            setIsLoading(false)
+        }
+        }
+
+        fetchContactsData()
+    }, [])
+
+        if(isLoading) {
+            return (
+                <article>
+                    <h1>Carregando contatos</h1>
+                </article>
+            )
+        }
+
+        if(err) {
+            return (
+                <article>
+                    <h1>Erro ao carregar dados.</h1>
+                    <p>Erro: {err}</p>
+                </article>
+            )
+        }
+
+
     return (
         <main>
             <h1>ContactFlow contacts page</h1>
+            <pre>{JSON.stringify(contacts, null, 2)}</pre>
+
+            {contacts.map((contact) => {
+                return (
+                    <ContactCard 
+                        contact={contact} 
+                        key={contact.id}
+                    />
+                )
+            })}
         </main>
     )
 }
