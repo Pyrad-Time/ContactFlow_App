39694fb578457883530529717f289b07540cffd5
Pyrad-Time <marcos0fataak@gmail.com>
2026-08-15 16:31:18 -0300

    feat(contacts): load contact details

--- front-end/src/pages/ContactDetails/index.jsx
+++ front-end/src/pages/ContactDetails/index.jsx
@@ -1,8 +1,53 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { getContactById } from "../../services/contactService"
+
 export function ContactDetails() {
+    const { id } = useParams()
+
+    const [ contact, setContact ] = useState(null)
+    const [ isLoading, setIsLoading ] = useState(true)
+    const [ errorMessage, setErrorMessage ] = useState("")
+
+    useEffect(() => {
+        async function loadContact() {
+            try {
+                setIsLoading(true)
+                setErrorMessage("")
+
+                const contactById = await getContactById(id)
+                setContact(contactById)
+
+            } catch (err){
+                console.error(err)
+                setErrorMessage("Could not load contact details.")
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        loadContact()
+    }, [id])
+
+    if(isLoading) {
+        return <p>Loading...</p>
+    }
+
+    if(errorMessage) {
+        return <p>Error: {errorMessage}</p>
+    }
+
+    if(!contact) {
+        return <p>Contact not found.</p>
+    }
     return ( 
         <main>
             <h1>Contact Details</h1>
-            <p>Contact details and interactions</p>
+            <p>Contact details and interactions {id} </p>
+            
+            <h2>Name: {contact.name}</h2>
+            <p>Email: {contact.email}</p>
+            <p>Phone: {contact.phone}</p>
         </main>
     )
 }
