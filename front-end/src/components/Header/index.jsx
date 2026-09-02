fc9d0530e580236128b713211d21f9b270d6f2e7
Pyrad-Time <marcos0fataak@gmail.com>
2026-09-02 15:38:46 -0300

    style(layout): add header navigation styles

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -2,17 +2,35 @@ import { NavLink } from "react-router-dom";
 
 export function Header() {
     return (
-        <header>
-            <nav>
-                <NavLink to="/dashboard">
+        <header className="appHeader">
+            <nav className="appNav">
+                <NavLink
+                    end
+                    className={({ isActive }) => 
+                        isActive ? "navLink navLinkActive" : "navLink"
+                    }
+                    to="/dashboard"
+                >
                     Dashboard
                 </NavLink>
 
-                <NavLink to="/contacts">
+                <NavLink 
+                    end
+                    className={({ isActive }) => 
+                        isActive ? "navLink navLinkActive" : "navLink"
+                    }
+                    to="/contacts"
+                >
                     Contacts
                 </NavLink>
 
-                <NavLink to="/contacts/new">
+                <NavLink 
+                    end
+                    className={({ isActive }) => 
+                        isActive ? "navLink navLinkActive" : "navLink"
+                    }
+                    to="/contacts/new"
+                >
                     New Contact
                 </NavLink>
             </nav>
